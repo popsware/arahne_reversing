b5f82c7a47a68512cec77f3324a9463467ce9d60
Mohab <mohabayman@hotmail.com>
2021-10-07 12:20:51 +0200

    nole7 ready

--- parseFiles.bat
+++ parseFiles.bat
@@ -9,19 +9,27 @@
 @REM py readFile.py "nole9" "cobrab-9-1.ep"
 @REM py readFile.py "nole9" "cobrab-9-0.ep"
 
+@REM py readFile.py "nole10" "cobrab-10-1.ep"
+@REM py readFile.py "nole10" "cobrab-10-0.ep"
 
+@REM py readFile.py "nole7" "cobrab-7-1.ep"
+@REM py readFile.py "nole7" "cobrab-7-0.ep"
+
+py readFile.py "nole6" "cobrab-6-1.3072"
+py readFile.py "nole6" "cobrab-6-0.3072"
+
+@REM py readFile.py "nole5" "cobrab-5-1.3072"
+@REM py readFile.py "nole5" "cobrab-5-0.3072"
+
+@REM py readFile.py "nole3" "cobrab-3-1.2688"
+@REM py readFile.py "nole3" "cobrab-3-0.2688"
+
+@REM py readFile.py "nole2" "cobrab-2-1.6144"
+@REM py readFile.py "nole2" "cobrab-2-0.6144"
 
 @REM py readFile.py "nole1" "cobrab-1-1.3072"
 @REM py readFile.py "nole1" "cobrab-1-0.3072"
 
 
-@REM py readFile.py "nole2" "cobrab-2-1.6144"
-@REM py readFile.py "nole2" "cobrab-2-0.6144"
-
-@REM py readFile.py "nole3" "cobrab-3-1.2688"
-@REM py readFile.py "nole3" "cobrab-3-0.2688"
 
 
-
-py readFile.py "nole10" "cobrab-10-1.ep"
-py readFile.py "nole10" "cobrab-10-0.ep"
